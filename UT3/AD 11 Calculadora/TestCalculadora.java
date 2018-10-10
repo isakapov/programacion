218c2f03d6e6656291c508f36563f6e1fedf7f39
Montse Viyuela <montsemsanz@gmail.com>
2018-10-10 17:54:10 +0200

    Añadidos de UT3 - Calculadora - Hora - Fecha

---
+++ UT3/AD 11 Calculadora/TestCalculadora.java
@@ -0,0 +1,28 @@
+
+/**
+ * Código para probar la clase Calculadora
+ * 
+*/
+import java.util.Scanner;
+public class TestCalculadora
+{
+	
+	public static void main (String args[])
+	{
+		Calculadora calculadora = new Calculadora();
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Introduzca numero: ");
+		int numero = teclado.nextInt();
+		while (numero !=0) 
+		{
+		    calculadora.addNumero(numero);
+		    System.out.print("Introduzca numero: ");
+		    numero = teclado.nextInt();
+	    }
+	    calculadora.printEstadisticas();
+		  
+	}
+
+	
+}
