d032c575e1d82a0d0973df2eae9e10288a5bcbff
Montse Viyuela <montsemsanz@gmail.com>
2019-04-02 15:38:35 +0200

    Añadido  Reproductor MP3 SOL

---
+++ UT7/ECL AD 7.2  Reproductor MP3 SOL/src/FicheroImagen.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * 
+ */
+public abstract class FicheroImagen extends Fichero
+{
+
+	/**
+	 *  
+	 */
+	public FicheroImagen(String titulo, int tamaño)
+	{
+		super(titulo, tamaño);
+	}
+
+	public void display()
+	{
+		super.display();
+	}
+
+}
---
+++ UT7/ECL AD 7.2  Reproductor MP3 SOL/src/FicheroMusica.java
@@ -0,0 +1,40 @@
+
+/**
+ * 
+ */
+public class FicheroMusica extends Fichero
+{
+	private int duracion;
+
+	/**
+	 * Constructor de la clase FicheroMusica
+	 */
+	public FicheroMusica(String titulo, int tamaño, int duracion)
+	{
+		super(titulo, tamaño);
+		this.duracion = duracion;
+
+	}
+
+	/**
+	 * devuelve la duración en segundos
+	 */
+	public int getDuracion()
+	{
+
+		return duracion;
+	}
+
+	/**
+	 * Muestra los datos del fichero de música La duración se muestra en formato
+	 * XX:XX (minutos, segundos)
+	 */
+	public void display()
+	{
+		super.display();
+		int min = getDuracion() / 60;
+		int seg = getDuracion() % 60;
+		System.out.println("Duración: " + min + " min " + seg + " seg");
+
+	}
+}
