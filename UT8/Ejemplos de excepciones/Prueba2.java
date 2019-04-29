ea9706b45606eef456aa474142989a9ecc7e9bc7
Montse Viyuela <montsemsanz@gmail.com>
2019-04-29 16:18:08 +0200

    Añadidos ejemplo excepciones - Proyecto Hora - Cuenta Excepciones

---
+++ UT8/Ejemplos de excepciones/Prueba2.java
@@ -0,0 +1,143 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+/**
+ *  class Prueba2  
+ *  
+ */
+public class Prueba2
+{
+
+    /**
+     *  
+     */
+    public void m1()
+    {
+        try    {
+            int[] array = new int[1];
+            array[0] = 10;
+            array[1] = 20;  // error
+            System.out.println("Esto no se imprimirá si hay error");
+
+        }
+        catch (ArrayIndexOutOfBoundsException e)   {
+            System.out.println("Error");
+        }
+        System.out.println("Fin");
+    }
+
+    /**
+     *  
+     */
+    public void m2()
+    {
+        try   {
+            int[] array = new int[1];
+            array[0] = 10;
+            array[1] = 20;  // error
+            System.out.println("Esto no se imprimirá si hay error");
+
+        }
+        catch (ArrayIndexOutOfBoundsException e)   {
+            System.out.println("Errror");
+        }
+        finally    {
+            System.out.println("en finally");
+        }
+        System.out.println("Fin");
+    }
+
+    /**
+     *  
+     */
+    public void m3()
+    {
+        try    {
+            int[] array = new int[1];
+            throw new IllegalArgumentException();
+            //             array[0] = 10;
+            //             array[1] = 20;  // error
+            //             System.out.println("Esto no se imprimirá si hay error");
+
+        }
+        catch (ArrayIndexOutOfBoundsException e)    {
+            System.out.println("Error");
+        }
+        finally    {
+            System.out.println("en finally");
+        }
+        System.out.println("Fin");
+    }
+
+    /**
+     *  
+     */
+    public void m4()
+    {
+        try     {
+            int[] array = new int[1];
+            throw new IllegalArgumentException();
+            //             array[0] = 10;
+            //             array[1] = 20;  // error
+            //             System.out.println("Esto no se imprimirá si hay error");
+
+        }
+        catch (ArrayIndexOutOfBoundsException e)  {
+            System.out.println("Error");
+        }
+
+        System.out.println("Fin");
+    }
+
+    /**
+     *  
+     */
+    public void m5()
+    {
+        Scanner teclado = new Scanner(System.in);
+        int num = 0;
+        boolean correcto = false;
+        while (!correcto)     {
+            try      { 
+                System.out.print("Teclee nº: ");
+                num = teclado.nextInt();            
+                System.out.println("Correcto "  + num);
+                correcto = true;
+            } 
+            catch (InputMismatchException ex )   { 
+                System.out.println("Incorrecto ");
+                teclado.next();
+            }
+
+        }
+
+    }
+
+    /**
+     *  When a scanner throws an InputMismatchException, 
+     *  the scanner will not pass the token that caused 
+     *  the exception, so that it may be retrieved or 
+     *  skipped via some other method. 
+     */
+    public void m7()
+    {
+        Scanner teclado = new Scanner(System.in);
+        int cantidad = 4;
+        int i = 1;
+        while (i <= cantidad)   {
+            try       { 
+                System.out.print("Teclee nº: ");
+                int num = teclado.nextInt();            
+                System.out.println("Correcto "  + num);
+                i++; 
+            } 
+            catch (InputMismatchException ex )   { 
+                System.out.println("Incorrecto ");
+                teclado.next();
+
+            }
+
+        }
+
+    }
+
+}
